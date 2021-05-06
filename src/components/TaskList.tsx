4f60aae557812563065196778ef9f5950d20e8e8
Matheus Pimentel <theusspimentel@gmail.com>
2021-05-06 20:57:44 -0300

    Desafio Finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -30,11 +30,11 @@ export function TaskList() {
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     const newTasks = tasks.map(task => task.id === id ? {
-       ...task, 
-      isComplete: !task.isComplete 
-    } : task)
+      ...task, 
+     isComplete: !task.isComplete 
+   } : task)
 
-      setTasks(newTasks)
+     setTasks(newTasks)
 
     
 
